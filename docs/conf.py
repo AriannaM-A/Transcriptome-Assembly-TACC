1cb42b702309f742b2b507341d38c4385230e4b0
AriannaM-A <ariannaaustin0403@gmail.com>
2025-10-21 11:55:06 -0500

    Update project information in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'De Novo Transcriptome Assembly'
+copyright = '2025, Austin'
+author = 'Austin'
 
 release = '0.1'
 version = '0.1.0'
